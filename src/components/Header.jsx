ee3a2c8025c550da3318ef27235a90e68e390add
Bhisma Upadhayaya <bhismarimal07@gmail.com>
2024-06-12 18:01:12 +0545

    header completed

---
+++ src/components/Header.jsx
@@ -0,0 +1,34 @@
+import { FaSearch } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <header className='bg-slate-300 shadow-emerald-500'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+           <Link to='/'>
+            <h1 className='font-bold text-sm sm:text-xl flex-wrap'>
+            <span className='text-pink-800'>PawPaws</span>
+            </h1>
+            </Link>
+            <form className='bg-gray-200 p-3 rounded-lg flex items-center'>
+                <input type= "text" 
+                placeholder ="Search..." 
+                className='bg-transparent focus:outline-none w-24 sm:w-64'></input>
+            </form>
+            <ul className='flex gap-4'>
+              <Link to='/'>
+              <li className='text-cyan-700 hidden sm:inline hover:underline'>Home</li>
+              </Link>
+              <Link to='/about'>
+              <li className='text-cyan-700 hidden sm:inline hover:underline'>About</li>
+              </Link>
+              <Link to='/sign-in'>
+              <li className='text-cyan-700 hidden sm:inline hover:underline'>SignIn</li>
+              </Link>
+              
+
+            </ul>
+            </div>
+      </header>
+  )
+}
