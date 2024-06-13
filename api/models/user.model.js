45317ec84ae2b8df1e972234b247242fb3d93355
Bhisma Upadhayaya <bhismarimal07@gmail.com>
2024-06-13 19:55:21 +0545

    signup api route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -2,23 +2,24 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     username: {
-        type : string,
+        type : String,
         required: true,
         unique: true,
         
     },
     email: {
-        type : string,
+        type : String,
         required: true,
         unique: true,
 
     },
     password: {
-        type : string,
+        type : String,
         required: true,
       
     }
 },{timestamps:true});
 
-const User = mongoose.model('User',userScehmma);
+const User = mongoose.model('User',userSchema);
+
 export default User;
