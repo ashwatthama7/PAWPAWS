41ad2cfcbf3d428ddf6ceba0c0da205eb8185899
Bhisma Upadhayaya <bhismarimal07@gmail.com>
2024-06-22 19:43:47 +0545

    create signin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -17,23 +17,23 @@ export const signup = async (req, res, next) => {
         next(error);
     }
 };
-/*
+
 export const signin = async (req, res, next) => {
     const{email, password} = req.body;
 
     try {
         const validUser = await User.findOne({email});
-        if(!validUser) 
-            return next(errorHandler(404, 'User not found!'));
-        
+        if (!validUser) return next(errorHandler(404, 'User not found!'));
         const vaildPassword = bcryptjs.compareSync(password, validUser.password);
-        
-        if(!vaildPassword)
-            return next(errorHandler(401,'Wrong Credentials'));
-        const token = jwt.sign{id: validUser._id }, process.env.JWT_SECRET;
+        if(!vaildPassword) return next(errorHandler(401,'Wrong Credentials'));
+        const token = jwt.sign({id: validUser._id }, process.env.JWT_SECRET);
+        const{password: pass, ...rest}= validUser._doc; //for hiding password from cookie
+        res
+        .cookie('access_token', token,{httpOnly: true})
+        .status(200)
+        .json({rest});//session and hiding password
     } catch (error) {
         next(error);
     }
    
 };
-*/
