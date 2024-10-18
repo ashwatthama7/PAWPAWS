07c8be2d4ada66464a0158a9e92d29bf517088c5
Bhisma Upadhayaya <bhismarimal07@gmail.com>
2024-10-18 06:24:16 +0545

    profile page ui 1

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,8 +2,9 @@ import { FaSearch } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
+
 export default function Header() {
-  const { currentUser } = useSelector((state) => state.user)
+  const { currentUser } = useSelector(state => state.user)
 
   return (
     <header className="bg-slate-300 shadow-emerald-500">
