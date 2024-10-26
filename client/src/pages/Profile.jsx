38883a25408b089d2ebb48bdb73e0ba841a2f56f
Bhisma Upadhayaya <bhismarimal07@gmail.com>
2024-10-26 06:11:03 +0545

    complete update functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { useRef, useState, useEffect } from 'react'
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useRef, useState, useEffect } from 'react';
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 import { app } from '../firebase';
+import { updateUserStart, updateUserSuccess,updateUserFailure } from '../redux/user/useSlice';
+
+
 
 
 export default function Profile() {
   const fileRef = useRef(null);
-  const { currentUser } = useSelector((state) => state.user);
+  const { currentUser,loading,error } = useSelector((state) => state.user);
   const [ file, setFile]= useState(undefined)
   const [ filePerc, setFilePerc] = useState(0);
   const [ fileUploadError, setFileUploadError] = useState(false);
   const [ formData, setFormData] = useState({});
+  const [updateSuccess, setUpdateSuccess] = useState(false);
   
+  const dispatch = useDispatch();
  
 useEffect(()=> {
 if(file){
@@ -46,14 +51,43 @@ uploadTask.on(
   );
   };
 
+  const handleChange = (e) =>{
+    setFormData({...formData, [e.target.id]: e.target.value});
+  };
 
+  //for preventing page from refresh while submitting
+  const handleSubmit = async(e) =>{
+    e.preventDefault();
 
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`
+        , {
+        method: 'POST',
+        headers:{
+          'Content-Type': 'application/json',
+        },
+          body: JSON.stringify(formData),
+      });
+      
+      const data = await res.json();
+      if(data.success === false){
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+       dispatch(updateUserSuccess(data));
+       setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+    }
+  };
 
   //file array ma 0 passs garera first ma select gareko file matra line ho, for some murkh users who selects multiple files.
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
-      <form className='flex flex-col gap-4'>
+      <form onSubmit={handleSubmit}
+      className='flex flex-col gap-4'>
         
         <input onChange={(e)=>setFile(e.target.files[0])} type="file" ref={fileRef} hidden accept='image/*'></input>
         
@@ -78,21 +112,32 @@ uploadTask.on(
         </p>
 
       <input type='text' 
-        placeholder='username'
+        placeholder='username' 
+        defaultValue={currentUser.username}      
+        id='username'
         className='border p-3 rounded-lg'
-        id='username'></input>
+        onChange={handleChange}
+        ></input>
       
       <input type='email' 
         placeholder='email'
+        id='email'
+        defaultValue={currentUser.email}
         className='border p-3 rounded-lg'
-        id='email'></input>
-      
-      <input type='text' 
+        onChange={handleChange}
+      ></input>
+
+      <input type='password' 
         placeholder='password'
         className='border p-3 rounded-lg'
-        id='password'></input>
-      
-      <button className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-90 disabled:opacity-80 '>Update</button>
+        id='password'
+        onChange={handleChange}
+        ></input>
+        
+      <button 
+        disabled={loading}
+      className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-90 disabled:opacity-80 '>
+      {loading ? 'Loading...' : 'Update'}</button>
       
       
       </form>
@@ -104,7 +149,8 @@ uploadTask.on(
 
       </div>
 
-
+          <p className='text-red-700 mt-5' >{error ? error :''}</p>
+          <p className='text-green-700'>{updateSuccess ? 'User is updated successfully!!':''}</p>
     </div>
   )
 }
