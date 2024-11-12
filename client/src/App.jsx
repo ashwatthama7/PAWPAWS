7964a9801dd60a73c46fe4c65c8bf2e89ac0050f
Bhisma Upadhayaya <bhismarimal07@gmail.com>
2024-11-12 20:35:15 +0545

    complete image swiper

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,10 @@ import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing';
+import Listing from './pages/Listing';
+
+
+
 export default function App() {
   return (
     <BrowserRouter>
@@ -17,6 +21,9 @@ export default function App() {
       <Route path='/sign-in' element={<SignIn />}/>
       <Route path='/sign-up' element={<SignUp />}/>
       <Route path='/about' element={<About />}/>
+      <Route path='/listing/:listingId' element={<Listing />}/>
+
+
       <Route element={<PrivateRoute/>}>
       <Route path='/profile' element={<Profile />} />
       <Route path='/create-listing'element={<CreateListing />}></Route>
@@ -28,6 +35,12 @@ export default function App() {
     
     </BrowserRouter>
   );
-}/* <Route path='/update-listing/listingId'element={<UpdateListing />}></Route>
+}
+
+
+
+
+
+/*<Route path='/update-listing/listingId'element={<UpdateListing />}></Route>
 yesma bhayeko listingId maps to the listing of the dog.
 */
