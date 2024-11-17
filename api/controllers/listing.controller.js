e3de726b5ac5762aebae9a090ed0af5f87ad62f3
Bhisma Upadhayaya <bhismarimal07@gmail.com>
2024-11-17 10:29:57 +0545

    search api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -63,4 +63,42 @@ export const getListing = async(req, res, next) =>{
     } catch (error) {
         
     }
+};
+
+export const getListings = async(req, res, next)=>{
+    try {
+        const limit = parseInt(req.query.limit)||9;
+        const startIndex = parseInt(req.query.startIndex)||0;
+
+        let vaccined = req.query.vaccined;
+        
+        if(vaccined === undefined|| vaccined==='false'){
+            vaccined = {$in:[false, true]};  
+        }
+
+        let stray = req.query.stray;
+        
+        if(stray === undefined|| stray==='false'){
+            stray = {$in:[false, true]};  
+        }
+
+
+        const searchTerm = req.query.searchTerm || '';
+
+        const sort = req.query.sort || 'createdAt';
+
+        const order = req.query.order || 'desc';
+
+        const listings = await  Listing.find({
+            name:{$regex: searchTerm, $options:'i'},
+            vaccined,
+            stray,
+        }).sort(
+            {[sort]:order }
+        ).limit(limit).skip(startIndex)
+        return res.status(200).json(listings);
+
+    } catch (error) {
+        next(error); 
+    }
 }
